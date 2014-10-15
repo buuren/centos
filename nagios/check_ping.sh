f920cedda781438018258db5524fd0192e3fa0f5
Vladimir Kolesnik <vkolesni@gmail.com>
2014-10-15 21:28:29 +0300

    Update check_ping.sh

--- nagios/check_ping.sh
+++ nagios/check_ping.sh
@@ -6,16 +6,16 @@ server_ip='192.28.54.117'
 lost_packets=`ping -c 4 $server_ip | grep 'received' | awk -F ',' '{print $3}' | awk -F ' ' '{print $1}' | tr -d '%'`
 
 if (( $lost_packets == 100 )); then
-   echo "CRITICAL - $server_ip full packet loss ($lost_packets%)."
+   echo "CRITICAL - $server_ip no connection ($lost_packets%)."
    exit 2
 elif (( 76<=$lost_packets && $lost_packets<=99 )); then
    echo "CRITICAL - $server_ip severe packet loss ($lost_packets%)."
    exit 2
 elif (( 51<=$lost_packets && $lost_packets<=75 )); then
-   echo "Warning - $server_ip large packet loss ($lost_packets%)."
+   echo "Warning - $server_ip major packet loss ($lost_packets%)."
    exit 1
 elif (( 26<=$lost_packets && $lost_packets<=50 )); then
-   echo "Warning - $server_ip minor packet loss ($lost_packets%)."
+   echo "Warning - $server_ip medium packet loss ($lost_packets%)."
    exit 1
 elif (( 1<=$lost_packets && $lost_packets<=25 )); then
    echo "Warning - $server_ip minor packet loss ($lost_packets%)."
