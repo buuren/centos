59b603e0d5ce22e1361ccf79743439a89c78a360
Vladimir Kolesnik <vkolesni@gmail.com>
2014-10-17 13:24:48 +0300

    Update check_ping.sh

--- nagios/check_ping.sh
+++ nagios/check_ping.sh
@@ -1,29 +1,48 @@
 #!/bin/bash
 
-#hardcoded 
-server_ip='192.28.54.117'
+server_ip=$1
+amount_of_pings=$2
 
-lost_packets=`ping -c 4 $server_ip | grep 'received' | awk -F ',' '{print $3}' | awk -F ' ' '{print $1}' | tr -d '%'`
+if [ ! $# == 2 ]; then
+  echo "CRITICAL - Usage: $0 ip_address amount_of_packets"
+  exit 2
+fi
 
-if (( $lost_packets == 100 )); then
-   echo "CRITICAL - $server_ip no connection ($lost_packets%)."
-   exit 2
-elif (( 76<=$lost_packets && $lost_packets<=99 )); then
-   echo "CRITICAL - $server_ip severe packet loss ($lost_packets%)."
-   exit 2
-elif (( 51<=$lost_packets && $lost_packets<=75 )); then
-   echo "Warning - $server_ip major packet loss ($lost_packets%)."
-   exit 1
-elif (( 26<=$lost_packets && $lost_packets<=50 )); then
-   echo "Warning - $server_ip medium packet loss ($lost_packets%)."
-   exit 1
-elif (( 1<=$lost_packets && $lost_packets<=25 )); then
-   echo "Warning - $server_ip minor packet loss ($lost_packets%)."
-   exit 1
-elif (( $lost_packets == 0 )); then
-   echo "OK - $server_ip no packet loss ($lost_packets%)"
-   exit 0
+check_if_ip=$(echo $server_ip | grep '^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$')
+check_if_number=$(echo $amount_of_pings | grep -E '^[0-9]+$')
+
+if [ $check_if_ip ]; then
+   if [ $check_if_number ]; then
+
+        lost_packets=`ping -c $amount_of_pings -W 4 $server_ip | grep 'received' | awk -F ',' '{print $3}' | awk -F ' ' '{print $1}' | tr -d '%'`
+
+           if (( $lost_packets == 100 )); then
+              echo "CRITICAL - $server_ip no connection ($lost_packets%)."
+              exit 2
+           elif (( 76<=$lost_packets && $lost_packets<=99 )); then
+              echo "CRITICAL - $server_ip severe packet loss ($lost_packets%)."
+              exit 2
+           elif (( 51<=$lost_packets && $lost_packets<=75 )); then
+              echo "Warning - $server_ip major packet loss ($lost_packets%)."
+              exit 1
+           elif (( 26<=$lost_packets && $lost_packets<=50 )); then
+              echo "Warning - $server_ip medium packet loss ($lost_packets%)."
+              exit 1
+           elif (( 1<=$lost_packets && $lost_packets<=25 )); then
+              echo "Warning - $server_ip minor packet loss ($lost_packets%)."
+              exit 1
+           elif (( $lost_packets == 0 )); then
+              echo "OK - $server_ip no packet loss ($lost_packets%)"
+              exit 0
+           else
+              echo "WARNING - $server_ip unknown packet loss ($lost_packets%)"
+              exit 3
+           fi
+   else
+        echo "CRITICAL - Packet amount is not a number"
+        exit 2
+   fi
 else
-   echo "WARNING - $server_ip unknown packet loss ($lost_packets%)"
-   exit 3
+   echo "CRITICAL - $server_ip does not match ip address"
+   exit 2
 fi
